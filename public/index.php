8228546b42eef7c936f831a0823de7230ae66c6b
ackinty <ackinty@gmail.com>
2015-04-26 09:27:50 +0200

    Allow choosing iconNb and lineNb via url.

--- public/index.php
+++ public/index.php
@@ -14,29 +14,65 @@ $app = new \Slim\App();
 $app->register(new \Slim\Views\Twig(SYS_APP . '/tpl'));
 // ============ END BOOTSTRAP ===================
 
+function askWizard($iconNb, $lineNb) {
+    $iconsList = array();
+
+    $wizard = new Wizard();
+    $wizard->addIconDir(array(
+        '/carl-olsen',
+        '/delapouite',
+        '/felbrigg',
+        '/john-colburn',
+        '/john-redman',
+        '/lorc',
+        '/priorblue',
+        '/sbed',
+        '/viscious-speed',
+        '/willdabeast',
+    ));
+
+    $iconsList = $wizard->play($iconNb, $lineNb);
+    foreach ($iconsList as $lineKey => $line) {
+        foreach ($line as $iconKey => $icon) {
+            $iconsList[$lineKey][$iconKey] = str_replace(SYS_WEB, '', $icon);
+        }
+    }
+
+    return $iconsList;
+}
+
 // ============ ROUTING ===================
 $app->get(
     '/',
     function ($request, $response, $args) {
-        $wizard = new Wizard();
-        $wizard->addIconDir(array(
-            '/delapouite',
-        ));
-
-        $iconsList = $wizard->play();
-        foreach ($iconsList as $lineKey => $line) {
-            foreach ($line as $iconKey => $icon) {
-                $iconsList[$lineKey][$iconKey] = str_replace(SYS_WEB, '', $icon);
-            }
-        }
+        $iconsList = askWizard(3, 3);
 
         return $this['view']->render($response, 'index.twig', array(
             'iconsList' => $iconsList,
         ));
     }
 )->setName('home');
-// ============ END ROUTING ===================
+$app->get(
+    '/{iconNb}',
+    function ($request, $response, $args) {
+        $iconsList = askWizard($args['iconNb'], 3);
 
+        return $this['view']->render($response, 'index.twig', array(
+            'iconsList' => $iconsList,
+        ));
+    }
+);
+$app->get(
+    '/{iconNb}/{lineNb}',
+    function ($request, $response, $args) {
+        $iconsList = askWizard($args['iconNb'], $args['lineNb']);
+
+        return $this['view']->render($response, 'index.twig', array(
+            'iconsList' => $iconsList,
+        ));
+    }
+);
+// ============ END ROUTING ===================
 
 // Run app
 $app->run();
